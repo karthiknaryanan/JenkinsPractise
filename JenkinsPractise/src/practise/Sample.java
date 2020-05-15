cdd710fe02cf69b6e085d594bcf8717274affc3f
91701 <91701@LAPTOP-KFV0PSKR>
2020-05-15 20:08:31 +0530

    Sample Commit 1

---
+++ JenkinsPractise/src/practise/Sample.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("Hey Dhruv");
+		System.out.println("Hey Kohli");
+		System.out.println("Hey Dhoni");
+		System.out.println("Hey Suresh");
+
+	}
+
+}
